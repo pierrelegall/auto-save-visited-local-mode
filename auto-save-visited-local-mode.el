e431f5f00993eb5e0f28f068f11e247e455e844b
Pierre Le Gall <pierre@legall.im>
2025-10-12 22:56:23 +0200

    Proper licensing

--- auto-save-visited-local-mode.el
+++ auto-save-visited-local-mode.el
@@ -1,6 +1,6 @@
 ;;; auto-save-visited-local-mode.el --- Buffer-local auto-save for visited files -*- lexical-binding: t -*-
 
-;; Copyright (C) 2025 Pierre <pierre@legall.im
+;; Copyright (C) 2025 Pierre <pierre@legall.im>
 
 ;; Author: Pierre Le Gall <pierre@legall.im>
 ;; Maintainer: Pierre <pierre@legall.im>
@@ -8,6 +8,22 @@
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: convenience files autosave
 ;; URL: https://github.com/pierrelegall/auto-save-visited-local-mode
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;; A buffer-local alternative to auto-save-visited-mode that saves
